52494cc3c67ca068207e1983b64432e0e5fe134f
Katie Booth <katiebooth3@gmail.com>
2023-01-07 17:51:20 +0000

    return json object instead

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ express = require('express')
 app = express()
 
 app.get('/', function(req,res){
-    res.send("Hello world")
+    res.json({Message :"Hello world"})
 })
 
 app.listen(9000, function () {
