d1ae3e60c04666f59c0a6afeed81a9d217ffa928
Katie Booth <katiebooth3@gmail.com>
2023-01-07 19:00:56 +0000

    fixed connection

--- server/create_router.js
+++ server/create_router.js
@@ -1,15 +1,17 @@
-const express = require('express')
+const express = require("express")
 
-const createRouter = function (collection) {
+const createRouter = function(collection){
 
     const router = express.Router()
 
-    router.get('/found', (req, res) => {
-        collection.find().toArray()
+    router.get("/all", (req, res) => {
+        collection
+        .find()
+        .toArray()
         .then(docs => res.json(docs))
         .catch((err) => {
             console.error(err);
-          })
+        })
     });
   
     return router;
--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require("mongodb").MongoClient
 const createRouter = require('./create_router')
 
 app.use(cors())
 app.use(express.json())
-MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
+MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true })
     .then((client)=>{
-        const db = client.db("lost_and_found")
+        const db = client.db("lost_and_found_items")
         const foundItems = db.collection("found_items")
         const foundItemsRouter = createRouter(foundItems)
         app.use('/found',foundItemsRouter)
